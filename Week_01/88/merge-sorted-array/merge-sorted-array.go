8fba054c3a448d47dba26b1a6ce80909bd472036
57Char <63660255@qq.com>
2020-04-19 12:11:52 +0800

    Add 88. Merge Sorted Array

---
+++ Week_01/88/merge-sorted-array/merge-sorted-array.go
@@ -0,0 +1,31 @@
+package merge_sorted_array
+
+import (
+	"sort"
+)
+
+// https://leetcode.com/problems/merge-sorted-array/
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i := m + n - 1
+	for i >= 0 && n > 0 {
+		if m > 0 && nums1[m-1] > nums2[n-1] {
+			nums1[i] = nums1[m-1]
+			m--
+		} else {
+			nums1[i] = nums2[n-1]
+			n--
+		}
+		i--
+	}
+}
+
+func mergeV2(nums1 []int, m int, nums2 []int, n int) {
+	//more := len(nums1) - m - n
+	nums1 = append(nums1[:m], nums2...)
+	sort.Ints(nums1)
+	//if more > 0 {
+	//	nums3 := make([]int, more)
+	//	nums1 = append(nums1, nums3...)
+	//}
+}
