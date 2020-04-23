44643458efe67cafc3e7d0e3ec22b4c87d990694
57Char <63660255@qq.com>
2020-04-23 23:13:34 +0800

    Add 94. Binary Tree Inorder Traversal

---
+++ Week_02/94/binary-tree-inorder-traversal/binary-tree-inorder-traversal.go
@@ -0,0 +1,33 @@
+package binary_tree_inorder_traversal
+
+// https://leetcode.com/problems/binary-tree-inorder-traversal
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	res := []int{}
+	inorder(root, &res)
+	return res
+}
+
+func inorder(root *TreeNode, res *[]int) {
+	if root == nil {
+		return
+	}
+	inorder(root.Left, res)
+	*res = append(*res, root.Val)
+	inorder(root.Right, res)
+}
+
+func inorderTraversalV2(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	return append(append(inorderTraversal(root.Left), root.Val), inorderTraversal(root.Right)...)
+}
