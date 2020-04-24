a8a1b0c34c996d55b06d89d2e3cb7f368f4f253e
57Char <63660255@qq.com>
2020-04-24 21:17:50 +0800

    Add 104. Maximum Depth of Binary Tree

---
+++ Week_02/everyday/104/maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.go
@@ -0,0 +1,48 @@
+package maximum_depth_of_binary_tree
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func maxDepthV2(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	depth := 0
+	level := []*TreeNode{root}
+	for len(level) > 0 {
+		depth++
+		q := []*TreeNode{}
+		for _, node := range level {
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+		level = q
+	}
+	return depth
+}
