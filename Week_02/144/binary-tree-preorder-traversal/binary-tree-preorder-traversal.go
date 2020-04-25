a75ac970b1f40295c705520d6724c9a837af9e0d
57Char <63660255@qq.com>
2020-04-25 11:57:19 +0800

    Add 144. Binary Tree Preorder Traversal

---
+++ Week_02/144/binary-tree-preorder-traversal/binary-tree-preorder-traversal.go
@@ -0,0 +1,49 @@
+package binary_tree_preorder_traversal
+
+// https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+func preorderTraversal(root *TreeNode) []int {
+	res := []int{}
+	preorder(root, &res)
+	return res
+}
+
+func preorder(root *TreeNode, res *[]int) {
+	if root == nil {
+		return
+	}
+	*res = append(*res, root.Val)
+	preorder(root.Left, res)
+	preorder(root.Right, res)
+}
+
+func preorderTraversalV2(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	return append(append([]int{root.Val}, preorderTraversal(root.Left)...), preorderTraversal(root.Right)...)
+}
+
+func preorderTraversalV3(root *TreeNode) []int {
+	res := []int{}
+	stack := []*TreeNode{}
+	node := root
+	for node != nil {
+		res = append(res, node.Val)
+		if node.Left != nil {
+			stack = append(stack, node)
+			node = node.Left
+		} else if node.Right != nil {
+			node = node.Right
+		} else {
+			node = nil
+			for i := len(stack) - 1; i >= 0; i-- {
+				if node = stack[i].Right; node != nil {
+					stack = stack[:i]
+					break
+				}
+			}
+		}
+	}
+	return res
+}
