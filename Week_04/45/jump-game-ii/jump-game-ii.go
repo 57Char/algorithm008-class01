e5fb1822e88de14abd099ebaeee401b51a1cebb8
57Char <63660255@qq.com>
2020-05-17 19:12:06 +0800

    Update 45. Jump Game II

--- Week_04/45/jump-game-ii/jump-game-ii.go
+++ Week_04/45/jump-game-ii/jump-game-ii.go
@@ -3,12 +3,27 @@ package jump_game_ii
 // https://leetcode.com/problems/jump-game-ii/
 
 func jump(nums []int) int {
-	res, end, step, n := 0, 0, 0, len(nums)
+	res, end, maxPosition, n := 0, 0, 0, len(nums)
 	for i := 0; i < n-1; i++ {
-		step = max(step, i+nums[i])
+		maxPosition = max(maxPosition, i+nums[i])
 		if i == end {
-			end = step
+			end = maxPosition
 			res++
+		}
+	}
+	return res
+}
+
+func jumpV2(nums []int) int {
+	res, end, maxPosition, n := 0, 0, 0, len(nums)
+	for i := 0; i < n-1; i++ {
+		maxPosition = max(maxPosition, i+nums[i])
+		if i == end {
+			end = maxPosition
+			res++
+		}
+		if end >= n {
+			break
 		}
 	}
 	return res
