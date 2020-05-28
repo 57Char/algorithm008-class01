77a3c4a1e429d590a4509705b7ed6e28041834b7
57Char <63660255@qq.com>
2020-05-28 23:42:26 +0800

    Update 120. Triangle

--- Week_06/everyday/120/triangle/triangle.go
+++ Week_06/everyday/120/triangle/triangle.go
@@ -2,7 +2,7 @@ package triangle
 
 // https://leetcode.com/problems/triangle/
 
-// Runtime: 4 ms, faster than 95.54% of Go online submissions for Triangle.
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Triangle.
 // Memory Usage: 3.1 MB, less than 100.00% of Go online submissions for Triangle.
 func minimumTotal(triangle [][]int) int {
 	n := len(triangle)
@@ -15,7 +15,8 @@ func minimumTotal(triangle [][]int) int {
 	for i := n - 2; i >= 0; i-- {
 		m := len(triangle[i])
 		for j := 0; j < m; j++ {
-			triangle[i][j] = triangle[i][j] + min(triangle[i+1][j], triangle[i+1][j+1])
+			//triangle[i][j] = triangle[i][j] + min(triangle[i+1][j], triangle[i+1][j+1])
+			triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
 		}
 	}
 	return triangle[0][0]
