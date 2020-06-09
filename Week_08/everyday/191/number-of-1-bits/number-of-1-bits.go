61c9a2460e149b021389e884996510ac2625932f
57Char <63660255@qq.com>
2020-06-09 22:50:07 +0800

    Add 191. Number of 1 Bits

---
+++ Week_08/everyday/191/number-of-1-bits/number-of-1-bits.go
@@ -0,0 +1,12 @@
+package number_of_1_bits
+
+// https://leetcode.com/problems/number-of-1-bits/
+
+func hammingWeight(num uint32) int {
+	var count uint32
+	for num > 0 {
+		count += num & 1
+		num >>= 1
+	}
+	return int(count)
+}
