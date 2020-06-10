ee3cfb4300f59879c188410534514c262cd91640
57Char <63660255@qq.com>
2020-06-11 00:22:21 +0800

    Add 1122. Relative Sort Array

---
+++ Week_08/everyday/1122/relative-sort-array/relative-sort-array.go
@@ -0,0 +1,19 @@
+package relative_sort_array
+
+import "sort"
+
+// https://leetcode.com/problems/relative-sort-array/
+
+func relativeSortArray(arr1 []int, arr2 []int) []int {
+	i, n := 0, len(arr1)
+	for _, v := range arr2 {
+		for j := 0; j < n; j++ {
+			if v == arr1[j] {
+				arr1[i], arr1[j] = arr1[j], arr1[i]
+				i++
+			}
+		}
+	}
+	sort.Ints(arr1[i:])
+	return append(arr1[:i], arr1[i:]...)
+}
