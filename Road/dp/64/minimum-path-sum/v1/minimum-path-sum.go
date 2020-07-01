6cddacac68e79081770a3d9e5c2c88d5c713ee52
57Char <63660255@qq.com>
2020-07-01 23:50:31 +0800

    Add 64. Minimum Path Sum

---
+++ Road/dp/64/minimum-path-sum/v1/minimum-path-sum.go
@@ -0,0 +1,32 @@
+package v1
+
+// https://leetcode.com/problems/minimum-path-sum/
+// 64. Minimum Path Sum
+
+func minPathSum(grid [][]int) int {
+	m := len(grid)
+	if m == 0 {
+		return 0
+	}
+	n := len(grid[0])
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i > 0 && j > 0 {
+				grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+			} else if i > 0 && j == 0 {
+				grid[i][j] += grid[i-1][j]
+			} else if i == 0 && j > 0 {
+				grid[i][j] += grid[i][j-1]
+			}
+		}
+	}
+
+	return grid[m-1][n-1]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
