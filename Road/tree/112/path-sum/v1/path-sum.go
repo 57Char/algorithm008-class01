8a38d702eb90d303fe506faf9d29c2d740895b38
57Char <63660255@qq.com>
2020-07-12 23:48:24 +0800

    Add 112. Path Sum

---
+++ Road/tree/112/path-sum/v1/path-sum.go
@@ -0,0 +1,22 @@
+package v1
+
+// https://leetcode.com/problems/path-sum/
+// 112. Path Sum
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil && root.Val == sum {
+		return true
+	}
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+}
